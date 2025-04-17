0e04102b6f3392f3d7168266bb0e5bcd4b689f62
Mijil Kawuri <114597873+Mijil28@users.noreply.github.com>
2025-04-17 15:38:17 +0700

    Update comparator.h

--- Mcu/f051/Inc/comparator.h
+++ Mcu/f051/Inc/comparator.h
@@ -15,13 +15,13 @@
 //#define COMP_PA5 0b1010101
 
    // High speed comparator
-   #define COMP_PA1 0b1000001
-   #define COMP_PA6 0b1010001
+   //#define COMP_PA1 0b1000001
+   //#define COMP_PA6 0b1010001
 
   // High speed comparator
-   #define COMP_PA0 0b1100001
-   #define COMP_PA4 0b1000001
-   #define COMP_PA5 0b1010001
+  // #define COMP_PA0 0b1100001
+  // #define COMP_PA4 0b1000001
+   //#define COMP_PA5 0b1010001
 
 #include "main.h"
 
